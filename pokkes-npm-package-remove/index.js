0640ffad8ca1493fe9d88e0704ae5c7d8033bccd
Richard <forbannade@gmail.com>
2023-06-07 11:30:35 +0200

    removed stuff

--- pokkes-npm-package-remove/index.js
+++
@@ -1,9 +0,0 @@
-import { filterTodosByCondition } from 'pokkes-npm-package-utils';
-
-export function removeTodo(todos, todoToRemove) {
-    return todos.filter((todo) => todo.id !== todoToRemove.id);
-}
-
-export function removeTodoByCondition(todos, conditionFunc) {
-    return filterTodosByCondition(todos, conditionFunc);
-}
